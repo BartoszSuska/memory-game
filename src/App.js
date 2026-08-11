be2616386de5986126801d14b50e81b924c64383
Bartosz Suska <60578620+BartoszSuska@users.noreply.github.com>
2026-08-12 01:12:28 +0200

    cards maching

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  {"src": "/img/card_hearts_J.png"},
-  {"src": "/img/card_hearts_K.png"},
-  {"src": "/img/card_hearts_Q.png"},
-  {"src": "/img/card_spades_J.png"},
-  {"src": "/img/card_spades_K.png"},
-  {"src": "/img/card_spades_Q.png"}
+  {"src": "/img/card_hearts_J.png", matched: false},
+  {"src": "/img/card_hearts_K.png", matched: false},
+  {"src": "/img/card_hearts_Q.png", matched: false},
+  {"src": "/img/card_spades_J.png", matched: false},
+  {"src": "/img/card_spades_K.png", matched: false},
+  {"src": "/img/card_spades_Q.png", matched: false}
 ]
 
 function App() {
 
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   //shuffle cards
   const shuffleCards = () => {
@@ -26,7 +28,38 @@ function App() {
       setTurns(0)
   }
 
-  console.log(cards, turns)
+  //handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  //compare 2 selected cards
+  useEffect(() => {
+    if(choiceOne && choiceTwo) {
+      if(choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map( card => {
+            if(card.src === choiceOne.src){
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        console.log('those cards do not match!')
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  //reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
 
   return (
     <div className="App">
@@ -35,7 +68,11 @@ function App() {
 
       <div className="card-grid">
         {cards.map(card => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard 
+            key={card.id} 
+            card={card} 
+            handleChoice={handleChoice} 
+          />
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,11 +1,21 @@
 import './SingleCard.css'
 
-export default function SingleCard({card}) {
+export default function SingleCard({card, handleChoice}) {
+
+    const handleClick = () => {
+        handleChoice(card)
+    }
+
     return (
         <div className="card" key={card.id}>
             <div>
                 <img className="front" src={card.src} alt="card front" />
-                <img className="back" src="/img/card_back.png" alt="card back" />
+                <img 
+                    className="back" 
+                    src="/img/card_back.png" 
+                    onClick={handleClick} 
+                    alt="card back" 
+                />
             </div>
         </div>
     )
